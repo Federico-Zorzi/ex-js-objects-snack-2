158e42b7b01695e29b70672c194892dfcd79b62b
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-24 17:23:40 +0100

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -10,7 +10,6 @@ const secondBurger = {...hamburger};
 secondBurger.ingredients[0] = "Salad";​
 
 console.log(hamburger.ingredients[0]); // ?
-
 console.log(secondBurger.ingredients[0]); // ?
 
 P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
@@ -18,3 +17,26 @@ P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi d
     Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
     Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
  */
+
+const hamburger = {
+  name: "Cheese Burger",
+  weight: 250,
+  ingredients: ["Cheese", "Meat", "Bread", "Tomato"],
+};
+
+const secondBurger = { ...hamburger };
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // "Salad"
+
+console.log(secondBurger.ingredients[0]); // "Salad"
+
+/* 
+- Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+    Il valore che avrà l'indice 0 dell'array sia in hamburger che in secondBurger sarà "Salad"
+    
+
+- Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+    Sono stati creati due oggetti il primo hamburger e il secondo secondBurger
+  
+*/
