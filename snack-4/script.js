3faf01eebbe5795eefc363f61d18bb5660d3d196
Federico-Zorzi <federico.zorzi05@gmail.com>
2025-02-24 17:35:36 +0100

    snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -6,22 +6,48 @@ const chef = {
     makeBurger: (num = 1) => {
         console.log(`Ecco ${num} hamburger per te!`);
     },
-
 }
-​
+    ​
 const restaurant = {
     name: "Hyur's Burgers",
     address: {
         street: 'Main Street',
         number: 123,
     },
-
     openingDate: new Date(2025, 3, 11),
-
     isOpen: false,
-
 };
 
     Qual è il metodo migliore per clonare l’oggetto chef, e perché?
     Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
+*/
+
+const chef = {
+  name: "Chef Hyur",
+  age: 29,
+  makeBurger: (num = 1) => {
+    console.log(`Ecco ${num} hamburger per te!`);
+  },
+};
+
+const restaurant = {
+  name: "Hyur's Burgers",
+  address: {
+    street: "Main Street",
+    number: 123,
+  },
+  openingDate: new Date(2025, 3, 11),
+  isOpen: false,
+};
+
+/* 
+- Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+    Il metodo migliore per clonare l'oggetto chef è lo Spread Operator, perchè non essendo un oggetto complesso
+    ci permette di clonare la funzione all'interno del nuovo oggetto, cosa che JSON.stringify e structuredClone
+    non ci avrebbero permesso di fare
+
+- Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
+    Il metodo migliore per clonare l'oggetto chef è structuredClone, perchè permette di clonare direttamente
+    sia la chiave address, che a sua volta è un oggetto e ci permette di clonare openingDate come oggetto,
+    senza doverlo convertire in stringa con JSON.stringify
 */
